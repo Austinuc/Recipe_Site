fc2924569a71e7b2e1cea994b12a08e18e823f2f
Austin <austin5astro@gmail.com>
2022-04-25 22:14:19 +0100

    Navigation bars completed; sticky and toggle UI completed

--- js/index.js
+++ js/index.js
@@ -8,9 +8,12 @@ window.onload=function(){
         event() {
             // alert("Toggle listener triggered");
             document.querySelector(".fa-bars").addEventListener('click', e => this.toggle());
+            document.querySelector(".fa-close").addEventListener('click', e => this.toggle());
         }
         toggle() {
             document.querySelector(".mobile-menu").classList.toggle("mobile-toggle");
+            document.querySelector(".fa-bars").classList.toggle("close");
+            document.querySelector(".fa-close").classList.toggle("close");
             // alert("Toggle ran successfully");
         }
     }
